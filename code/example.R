3b7888487b4a1b7e06a5b438a96b444ae7b7c11c
Alvaro Limber Chirino Gutierrez <alvaro.rqsbo@gmail.com>
2024-11-07 15:50:34 -0400

    Update example.R

--- code/example.R
+++ code/example.R
@@ -1,9 +1,8 @@
 rm(list=ls())
 library(ggplot2)
-da
-pdf("_fig_col/example1.pdf")
+pdf("_fig_col/example1.pdf", width = 11)
 ggplot(iris, aes(Petal.Length, fill=Species))+geom_density()
 dev.off()
-pdf("_fig_bn/example1.pdf")
+pdf("_fig_bn/example1.pdf", width = 11)
 ggplot(iris, aes(Petal.Length, fill=Species))+geom_density()+scale_fill_grey()
 dev.off()
